17777cd5683cecf597fdb72f7ae61981b0f050ed
Rut Vasner <rutivasner@gmail.com>
2024-01-09 23:33:33 +0200

    ruti_privateArea

--- controllers/mall.controllers.js
+++ controllers/mall.controllers.js
@@ -2,20 +2,23 @@ const { Mall } = require("../models/Mall.model");
 
 
 exports.getMalls = async (req, res, next) => {
+console.log("delete");
+
     const malls = await Mall.find({});
     res.send(malls);
 };
 
 exports.getMallById = async (req, res, next) => {
     const { id } = req.params;
-    console.log({id});
-    const mall = await Mall.find({id});
+    console.log({ id });
+    const mall = await Mall.find({ id });
     console.log(await mall);
     res.send(mall);
 };
 
 
 exports.postMall = async (req, res, next) => {
+
     const body = req.body;
     const owner_id = res.locals.place_id;
     try {
@@ -30,3 +33,24 @@ exports.postMall = async (req, res, next) => {
     }
 };
 
+exports.deleteMall = async (req, res) => {
+    // const mallId = req.params.id;
+console.log("delete");
+
+    // try   {
+
+        let data = await Mall.deleteOne({ _id: req.params.id })
+        //if success -n=1
+        res.json(data)
+
+        console.log(req)
+        // if (data.n === 1) {
+        //     console.log("Mall deleted successfully");
+        //     return res.json({ message: 'Mall deleted successfully' });
+        // } else {
+        //     console.log("Mall not found ");
+
+        //     return res.status(404).json({ message: 'Mall not found' });
+        // }
+  
+}
--- routes/mall.routes.js
+++ routes/mall.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { postMall, getMallById, getMalls } = require("../controllers/mall.controllers");
+const { postMall, getMallById, getMalls, deleteMall } = require("../controllers/mall.controllers");
 const { auth } = require("../middlewares/auth");
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.get("/", getMalls);
 router.get("/:id", getMallById);
 
 router.post("/", auth(), postMall);
+
+router.delete('/:id', deleteMall);
+
 
 module.exports = router;
